936b899c0622ae92231f3eb8809f90a89792f908
apoorvkh <mail@apoorvkh.com>
2024-05-01 21:54:47 -0400

    numpy support in det hash

--- dango/utils/det_hash.py
+++ dango/utils/det_hash.py
@@ -11,21 +11,13 @@ from uuid import UUID
 import ormsgpack
 from xxhash import xxh3_64_hexdigest
 
-# option=ormsgpack.OPT_SERIALIZE_NUMPY
-# TODO: numpy.ndarray
-# The array must be a contiguous C array (C_CONTIGUOUS) and one of the supported datatypes
-# else obj.tolist() can be specified
-# TODO: numpy.float64, numpy.float32, numpy.float16, numpy.int64, numpy.int32, numpy.int16, numpy.int8, numpy.uint64, numpy.uint32, numpy.uint16, numpy.uint8, numpy.uintp, numpy.intp, numpy.datetime64, and numpy.bool
-
+# TODO: check if numpy works and is consistent
+# TODO: np.float64 seems broken
 # TODO: torch.tensor
 # TODO: pydantic
 
 # TODO: patch arbitrary types
 # TODO: allow tagging classes with UUID
-
-
-class bigint:
-    pass
 
 
 def class_identifier(cls_or_obj: type | t.Any):
@@ -41,34 +33,62 @@ def class_identifier(cls_or_obj: type | t.Any):
     return f"{module_name}.{cls.__qualname__}"
 
 
+def is_bigint(i: int):
+    return (i > 0 and i.bit_length() > 64) or (i < 0 and i.bit_length() > 63)
+
+
+class bigint:
+    pass
+
+
+try:
+    import numpy as np
+
+    SERIALIZABLE_NUMPY_TYPES = (
+        np.float64,
+        np.float32,
+        np.float16,
+        np.int64,
+        np.int32,
+        np.int16,
+        np.int8,
+        np.uint64,
+        np.uint32,
+        np.uint16,
+        np.uint8,
+        np.uintp,
+        np.intp,
+        np.datetime64,
+        np.bool_,
+    )
+
+    def is_np_array(o) -> bool:
+        return isinstance(o, np.ndarray) and o.dtype.type in SERIALIZABLE_NUMPY_TYPES
+
+    def is_np_type(o) -> bool:
+        return type(o) in SERIALIZABLE_NUMPY_TYPES
+
+except ImportError:
+
+    def is_np_array(o) -> bool:
+        return False
+
+    def is_np_type(o) -> bool:
+        return False
+
+
 def det_hash(obj, seed: int = 0) -> str:
     obj_type = class_identifier(obj)
-
-    # for serializing larger than 64-bit integers
-    if isinstance(obj, int) and (
-        (obj > 0 and obj.bit_length() > 64) or (obj < 0 and obj.bit_length() > 63)
-    ):
-        obj_type = class_identifier(bigint)
-        obj = str(obj)
-
-    ##
-
     obj_data = None
-    native_type = True
+    ormsgpack_options = 0
 
     match obj:
-        case (
-            None
-            | bytes()
-            | str()
-            | int()
-            | float()
-            | bool()
-            | UUID()
-            | date()
-            | time()
-            | datetime()
-        ):
+        case None | bytes() | str() | float() | bool() | UUID() | date() | time() | datetime():
+            obj_data = obj
+        case int() if is_bigint(obj):
+            obj_type = class_identifier(bigint)
+            obj_data = str(obj)
+        case int():
             obj_data = obj
         case bytearray():
             obj_data = bytes(obj)
@@ -83,27 +103,30 @@ def det_hash(obj, seed: int = 0) -> str:
         case dict():
             obj_data = [(det_hash(k, seed=seed), det_hash(v, seed=seed)) for k, v in obj.items()]
             obj_data = sorted(obj_data, key=itemgetter(0))
-        case _:
-            native_type = False
-
-    if not native_type:
-        if deterministic_hashing.is_enabled() and vars(type(obj)).get("__hash__") is not None:
+        case _ if is_np_type(obj):
+            obj_data = obj
+            ormsgpack_options |= ormsgpack.OPT_SERIALIZE_NUMPY
+        case _ if is_np_array(obj) and obj.data.c_contiguous:
+            obj_type = (class_identifier(obj), class_identifier(obj.dtype.type))
+            obj_data = obj
+            ormsgpack_options |= ormsgpack.OPT_SERIALIZE_NUMPY
+        case _ if is_np_array(obj):
+            obj_data = obj.tolist()
+        case _ if deterministic_hashing.is_enabled() and vars(type(obj)).get("__hash__"):
             try:
                 obj_data = obj.__hash__()  # pyright: ignore [reportOptionalCall]
             except RecursionError as e:
                 raise RecursionError(
                     f"Circular reference likely exists in __hash__ function of {obj_type} class"
                 ) from e
-        elif is_dataclass(obj):
+        case _ if is_dataclass(obj):
             raise TypeError(
                 f"Set `frozen=True` or `unsafe_hash=True` in @dataclass decorator to make {obj_type} hashable"
             )
-        else:
+        case _:
             raise TypeError(f"Unhashable type: {obj_type}")
 
-    ##
-
-    serialized_data = ormsgpack.packb((obj_type, obj_data))
+    serialized_data = ormsgpack.packb((obj_type, obj_data), option=ormsgpack_options)
     return xxh3_64_hexdigest(serialized_data, seed=seed)
 
 
