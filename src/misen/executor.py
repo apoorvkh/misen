53069128f38c484e1ba875b66dcb6e78a610b03e
apoorvkh <mail@apoorvkh.com>
2024-11-10 16:17:42 -0500

    note about graph partitioning + added rustworkx dep

--- src/misen/executor.py
+++ src/misen/executor.py
@@ -9,6 +9,15 @@ if TYPE_CHECKING:
 
 
 class Executor(ABC):
+    def computable_groups(self, task: Task, workspace: Workspace):
+        """
+        This function should partition (s.t. `task` and every cache=True Task is the root of a partition).
+        Then we should return the graph of partitions. Each partition should represented as a topologically-sorted list of its Tasks.
+
+        I think we can use the rustworkx library if want to use efficient graph algorithms.
+        """
+        # TODO: implement this here
+
     def submit(self, task: Task, workspace: Workspace):
         raise NotImplementedError
 
