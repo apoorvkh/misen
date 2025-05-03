e876d253c9d5cb1b656c57ec008c5bbbae584146
apoorvkh <mail@apoorvkh.com>
2025-05-03 12:51:25 -0400

    experiment inherits tasks caching

--- src/misen/experiment.py
+++ src/misen/experiment.py
@@ -1,14 +1,14 @@
 from __future__ import annotations
 
 from abc import abstractmethod
-from typing import Any, Generic, Mapping, TypeVar
+from functools import cache
+from typing import Annotated, Generic, Mapping, TypeVar
 
 import tyro
 from msgspec import Struct
 
 from .executor import Executor
 from .task import Task
-from .utils.cached_property import cached_property
 from .workspace import Workspace
 
 TasksT = TypeVar("TasksT", bound=Mapping[str, Task])
@@ -17,25 +17,14 @@ TasksT = TypeVar("TasksT", bound=Mapping[str, Task])
 class Experiment(Generic[TasksT], Struct, frozen=True):
     @abstractmethod
     def tasks(self) -> TasksT:
-        raise NotImplementedError
+        raise NotImplementedError(f"{self.__class__.__name__} must implement a tasks() method.")
 
-    def __getattribute__(self, name: str) -> Any:
-        if name == "tasks":
-            return cached_property(self, type(self).tasks, key="__cached_task_graph__")
-        return super().__getattribute__(name)
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        setattr(cls, "tasks", cache(cls.tasks))
 
     def run(self, workspace: Workspace | None, executor: Executor | None) -> None:
-        Task((lambda **kwargs: None), **self.tasks()).run(
-            workspace=workspace, executor=executor
-        )
-
-    @classmethod
-    def _cli_run(
-        cls,
-        workspace: Workspace | None = None,
-        executor: Executor | None = None,
-    ) -> None:
-        cls().run(workspace=workspace, executor=executor)
+        Task((lambda **kwargs: None), **self.tasks()).run(workspace=workspace, executor=executor)
 
     @classmethod
     def cli(cls):
