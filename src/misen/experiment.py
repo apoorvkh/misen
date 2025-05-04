b0170f0c4396f277618a66fd0e0be231b63f2a65
apoorvkh <mail@apoorvkh.com>
2025-05-03 21:39:43 -0400

    updated CLI interface for experiments

--- src/misen/experiment.py
+++ src/misen/experiment.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
 from abc import abstractmethod
+from ast import literal_eval
 from functools import cache
-from typing import Annotated, Generic, Mapping, TypeVar
+from typing import Annotated, Generic, Literal, Mapping, TypeVar
 
 import tyro
 from msgspec import Struct
+from typing_extensions import Self
 
 from .executor import Executor
 from .task import Task
@@ -33,13 +35,27 @@ class Experiment(Generic[TasksT], Struct, frozen=True):
         Task((lambda **kwargs: None), **self.tasks()).run(workspace=workspace, executor=executor)
 
     @classmethod
-    def cli(cls):
-        executor, workspace, experiment = tyro.cli(
-            tuple[
-                Annotated[Executor, tyro.conf.arg(name="executor")],
-                Annotated[Workspace, tyro.conf.arg(name="workspace")],
-                Annotated[cls, tyro.conf.OmitArgPrefixes, tyro.conf.arg(name="experiment")],
-            ]
-        )
+    def _cli_entrypoint(
+        cls,
+        executor_params: Annotated[str | None, tyro.conf.arg(name="executor", default=None)],
+        workspace_params: Annotated[str | None, tyro.conf.arg(name="workspace", default=None)],
+        experiment: tyro.conf.OmitArgPrefixes[Self],
+        command: tyro.conf.Positional[Literal["run", "count"]] = "run",
+    ):
+        if executor_params is None:
+            executor = Executor.default()
+        else:
+            executor = Executor.from_params(literal_eval(executor_params))
 
-        experiment.run(workspace=workspace, executor=executor)
+        if workspace_params is None:
+            workspace = Workspace.default()
+        else:
+            workspace = Workspace.from_params(literal_eval(workspace_params))
+
+        match command:
+            case "run":
+                experiment.run(workspace=workspace, executor=executor)
+
+    @classmethod
+    def cli(cls):
+        tyro.cli(cls._cli_entrypoint)
