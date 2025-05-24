2ca3025224586af1cc2b4adcd0db3abb8e0a5fcd
apoorvkh <mail@apoorvkh.com>
2025-05-24 11:20:11 -0400

    clean up _distributable_tasks function

--- src/misen/executor.py
+++ src/misen/executor.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import abstractmethod
+from functools import cache
 from importlib import import_module
 from typing import TYPE_CHECKING, Literal
 
@@ -52,74 +53,66 @@ class Executor(Struct, kw_only=True):
 
     def computable_groups(self, task: Task, workspace: Workspace | None = None):
         workspace = workspace or Workspace.load()
-        return _computable_groups(task, workspace)
+        return _distributable_tasks(task, workspace)
 
     @abstractmethod
     def submit(self, task: Task, workspace: Workspace) -> Future:
         raise NotImplementedError
 
 
-def _computable_groups(root: Task, workspace: Workspace) -> dict[Task, list[Task]]:
+def _distributable_tasks(root: Task, workspace: Workspace) -> dict[Task, list[Task]]:
     """
-    This function should partition (s.t. `task` and every cache=True Task is the root of a partition).
-    Then we should return the graph of partitions (i.e. a contracted graph of Tasks).
-    """
-    import rustworkx
+    Given a DAG (represented by the root Task), this function should identify tasks that can be executed by distributed workers.
+    Specifically, these are the cacheable tasks. This function should return a dependency graph of just those tasks and the root.
 
-    hash_to_task: dict[int, Task] = {}
-    selected_nodes: set[int] = set()  # root and cacheable tasks
-    dag = rustworkx.PyDiGraph()
+    Implementation: build a DAG, then remove all (non-cacheable or non-root) nodes, while maintaining the dependency structure.
+    Returns: DAG in adjacency list format. Keys depend on values.
+    """
+    from rustworkx import PyDiGraph
+
+    dag = PyDiGraph()
 
     ### Build DAG via DFS
 
-    hash_to_node: dict[int, int] = {}
-    stack: list[Task] = [root]
-    task_is_cached: dict[int, bool] = {}
+    @cache
+    def _is_cached(task: Task) -> bool:
+        return task.is_cached_direct(workspace=workspace)
 
-    root_hash = root.__hash__()
-    root_node = dag.add_node(root_hash)
-    hash_to_node[root_hash] = root_node
-    selected_nodes.add(root_node)
+    task_node: dict[Task, int] = {root: dag.add_node(root)}
+    stack: list[Task] = [root]
+    visited: set[Task] = set()
 
     while stack:
         task: Task = stack.pop()
-        task_hash: int = task.__hash__()
-
-        # check if already visited; else visit
-        if task_hash in hash_to_task:
+        if task in visited:
             continue
-        hash_to_task[task_hash] = task
+        visited.add(task)
 
         # from prior iteration
-        node = hash_to_node[task_hash]
-
-        # select node if cacheable
-        if task.properties.cacheable:
-            selected_nodes.add(node)
+        node = task_node[task]
 
         # traverse children
         for dep in task.dependencies():
             dep: Task
-            dep_hash: int = dep.__hash__()
-
             # skip cached tasks
-            if dep_hash not in task_is_cached:
-                task_is_cached[dep_hash] = dep.is_cached_direct(workspace=workspace)
-            if task_is_cached[dep_hash]:
+            if _is_cached(dep):
                 continue
 
             # add dependent to graph if not already present
-            if dep_hash not in hash_to_node:
-                hash_to_node[dep_hash] = dag.add_node(dep_hash)
+            if dep not in task_node:
+                task_node[dep] = dag.add_node(dep)
                 stack.append(dep)
-            dep_node = hash_to_node[dep_hash]
+            dep_node = task_node[dep]
 
             # add edge from task to dep_task
             dag.add_edge(node, dep_node, None)  # args: parent, child, edge_data
 
-    ### Retain only selected tasks (and the induced graph)
+    ### Retain only root & cachable tasks (and the induced graph minor)
 
-    for node in set(dag.node_indices()) - selected_nodes:
+    nodes_to_remove = dag.filter_nodes(lambda task: not (task.properties.cacheable or task == root))
+    for node in nodes_to_remove:
         dag.remove_node_retain_edges(node)
 
-    return dag
+    ### Return as adjacency list
+
+    return {dag.get_node_data(node): dag.successors(node) for node in dag.node_indices()}
