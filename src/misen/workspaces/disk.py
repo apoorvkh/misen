cba17a55aba3195cf53034907611399dee7d2d49
Peter Curtin <peter_curtin@brown.edu>
2025-10-26 16:50:14 -0400

    refactor typing in disk workspace

--- src/misen/workspaces/disk.py
+++ src/misen/workspaces/disk.py
@@ -3,22 +3,21 @@ from __future__ import annotations
 import os
 from datetime import timedelta
 from pathlib import Path
-from typing import MutableMapping, cast
+from typing import MutableMapping, TypeVar, cast
 
 import lmdb
 from flufl.lock import Lock, NotLockedError  # pyright: ignore
 
+from ..task import Hash, ResolvedTaskHash, ResultHash, TaskHash
 from ..workspace import (
-    Hash,
-    ResolvedHashCacheABC,
-    ResultCacheABC,
-    ResultHash,
-    ResultHashCacheABC,
     Workspace,
 )
 
+KT = TypeVar("KT", bound=Hash)
+VT = TypeVar("VT", bound=Hash)
 
-class LMDBMapping(MutableMapping[Hash, Hash]):
+
+class LMDBMapping(MutableMapping[KT, VT]):
     def __init__(self, database_dir: Path):
         self.env = lmdb.open(str(database_dir), map_size=1_000_000_000, max_dbs=1)
         self.db = self.env.open_db()
@@ -26,21 +25,21 @@ class LMDBMapping(MutableMapping[Hash, Hash]):
     def __hash_encode(self, hash: Hash) -> bytes:
         return hash.to_bytes(8, byteorder="big")
 
-    def __hash_decode(self, encoded_hash: bytes) -> Hash:
-        return cast("Hash", int.from_bytes(encoded_hash, byteorder="big"))
+    def __hash_decode(self, encoded_hash: bytes, typ: type[Hash]) -> Hash:
+        return typ(int.from_bytes(encoded_hash, byteorder="big"))
 
-    def __getitem__(self, key: Hash) -> Hash:
+    def __getitem__(self, key: KT) -> VT:
         with self.env.begin(db=self.db) as txn:
             value = txn.get(self.__hash_encode(key))
             if value is None:
                 raise KeyError(f"Key {key} not found")
-            return self.__hash_decode(value)
+            return cast(VT, self.__hash_decode(value, self.__orig_class__.__args__[1]))  # type: ignore
 
-    def __setitem__(self, key: Hash, value: Hash):
+    def __setitem__(self, key: KT, value: VT):
         with self.env.begin(db=self.db, write=True) as txn:
             txn.put(self.__hash_encode(key), self.__hash_encode(value))
 
-    def __delitem__(self, key: Hash):
+    def __delitem__(self, key: KT):
         with self.env.begin(db=self.db, write=True) as txn:
             if not txn.delete(self.__hash_encode(key)):
                 raise KeyError(f"Key {key} not found")
@@ -49,30 +48,30 @@ class LMDBMapping(MutableMapping[Hash, Hash]):
         with self.env.begin(db=self.db) as txn:
             cursor = txn.cursor()
             for encoded_key, _ in cursor:
-                yield self.__hash_decode(encoded_key)
+                yield cast("KT", self.__hash_decode(encoded_key, self.__orig_class__.__args__[0]))  # type: ignore
 
     def __len__(self):
         with self.env.begin(db=self.db) as txn:
             return txn.stat(db=self.db)["entries"]
 
 
-class DiskResolvedHashCache(ResolvedHashCacheABC):
-    def __init__(self, workspace: DiskWorkspace):
-        super().__init__()
-        self.workspace = workspace
-        self.mapping = LMDBMapping(self.workspace.workspace_directory / "resolved_hash_cache")  # pyright: ignore
+# class DiskResolvedHashCache(MutableMapping[TaskHash, ResolvedTaskHash]):
+#     def __init__(self, workspace: DiskWorkspace):
+#         super().__init__()
+#         self.workspace = workspace
+#         self.mapping = LMDBMapping(self.workspace.workspace_directory / "resolved_hash_cache")  # pyright: ignore
 
 
-class DiskResultHashCache(ResultHashCacheABC):
-    def __init__(self, workspace: DiskWorkspace):
-        super().__init__()
-        self.workspace = workspace
-        self.mapping = LMDBMapping(self.workspace.workspace_directory / "result_hash_cache")  # pyright: ignore
+# class DiskResultHashCache(MutableMapping[ResolvedTaskHash, ResultHash]):
+#     def __init__(self, workspace: DiskWorkspace):
+#         super().__init__()
+#         self.workspace = workspace
+#         self.mapping = LMDBMapping(self.workspace.workspace_directory / "result_hash_cache")  # pyright: ignore
 
 
 class DiskResultCacheMapping(MutableMapping[ResultHash, bytes]):
-    def __init__(self, cache: DiskResultCache):
-        self.cache = cache
+    def __init__(self, result_cache_directory: Path):
+        self.result_cache_directory = result_cache_directory
 
     def __aquire_lock(self, filename: Path) -> Lock:
         item_lock_filename = filename.name + ".lock"
@@ -83,7 +82,7 @@ class DiskResultCacheMapping(MutableMapping[ResultHash, bytes]):
         return lock
 
     def __get_key_filename(self, key: ResultHash) -> Path:
-        return self.cache.result_cache_directory / f"result_{str(key)}"
+        return self.result_cache_directory / f"result_{str(key)}"
 
     def __setitem__(self, key: ResultHash, value: bytes):
         item_filename = self.__get_key_filename(key)
@@ -138,30 +137,26 @@ class DiskResultCacheMapping(MutableMapping[ResultHash, bytes]):
         # do not remove the lock file for now, it has a race condition
 
     def __iter__(self):
-        for path in self.cache.result_cache_directory.iterdir():
+        for path in self.result_cache_directory.iterdir():
             if path.is_file() and path.name.startswith("result_"):
                 yield ResultHash(path.name[7:])
 
     def __len__(self) -> int:
         return len(
-            list(
-                filter(
-                    lambda x: x.startswith("result_"), os.listdir(self.cache.result_cache_directory)
-                )
-            )
+            list(filter(lambda x: x.startswith("result_"), os.listdir(self.result_cache_directory)))
         )
 
 
-class DiskResultCache(ResultCacheABC):
-    def __init__(self, workspace: DiskWorkspace, new_workspace: bool = False):
-        super().__init__()
-        self.workspace = workspace
-        self.result_cache_directory = workspace.workspace_directory / "result_cache"
+# class DiskResultCache(MutableMapping[ResultHash, bytes]):
+#     def __init__(self, workspace: DiskWorkspace, new_workspace: bool = False):
+#         super().__init__()
+#         self.workspace = workspace
+#         self.result_cache_directory = workspace.workspace_directory / "result_cache"
 
-        if new_workspace:
-            os.mkdir(self.result_cache_directory)
+#         if new_workspace:
+#             os.mkdir(self.result_cache_directory)
 
-        self.mapping = DiskResultCacheMapping(self)
+#         self.mapping = DiskResultCacheMapping(self)
 
 
 class DiskWorkspace(Workspace):
@@ -171,11 +166,22 @@ class DiskWorkspace(Workspace):
         self.workspace_directory = self.project_directory / ".misen_workspace"
 
         # at the moment, only support one workspace per directory, TODO: support multiple
-        new_workspace = False
         if not self.workspace_directory.exists():
             os.mkdir(self.workspace_directory)
-            new_workspace = True
 
-        self.resolved_hashes = DiskResolvedHashCache(workspace=self)
-        self.result_hashes = DiskResultHashCache(workspace=self)
-        self.results = DiskResultCache(workspace=self, new_workspace=new_workspace)
+        super().__init__(
+            resolved_hash_cache=LMDBMapping[TaskHash, ResolvedTaskHash](
+                database_dir=self.workspace_directory / "resolved_hash_cache"
+            ),
+            result_hash_cache=LMDBMapping[ResolvedTaskHash, ResultHash](
+                database_dir=self.workspace_directory / "result_hash_cache"
+            ),
+            result_cache=DiskResultCacheMapping(
+                result_cache_directory=self.workspace_directory / "result_cache"
+            ),
+            log_store={},
+        )
+
+        # self.resolved_hashes = DiskResolvedHashCache(workspace=self)
+        # self.result_hashes = DiskResultHashCache(workspace=self)
+        # self.results = DiskResultCache(workspace=self, new_workspace=new_workspace)
