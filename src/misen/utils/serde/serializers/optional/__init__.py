241f185a7eeb2cde4ef0e8157d8bc23a9ad6c30e
apoorvkh <mail@apoorvkh.com>
2026-04-09 21:56:45 -0400

    Drop niche serializers, keep only types with stable formats
    Remove 29 serializers for libraries where serialization is either
    pickle-based (sklearn, torch.nn.Module, keras) or too niche for misen
    to maintain (altair, bokeh, beautifulsoup, lxml, graphviz, pptx, docx,
    openpyxl, pydub, spacy, transformers, etc.). Users can provide custom
    serializers for these via @task(serializer=...).
    
    Remaining optional serializers cover types with genuinely stable,
    non-pickle formats: numpy (.npy), torch Tensor (.pt weights_only),
    pandas/polars/pyarrow (parquet), scipy sparse (.npz), Pillow (PNG),
    pydantic (JSON), msgspec (msgpack), attrs (dict + msgpack).

--- src/misen/utils/serde/serializers/optional/__init__.py
+++ src/misen/utils/serde/serializers/optional/__init__.py
@@ -1,54 +1,19 @@
 """Aggregate optional serializers for third-party library types."""
 
 from misen.utils.serde.serializer_base import SerializerTypeList, SerializerTypeRegistry
-from misen.utils.serde.serializers.optional.altair import altair_serializers, altair_serializers_by_type
 from misen.utils.serde.serializers.optional.attrs import attrs_serializers, attrs_serializers_by_type
-from misen.utils.serde.serializers.optional.beautifulsoup import (
-    beautifulsoup_serializers,
-    beautifulsoup_serializers_by_type,
-)
-from misen.utils.serde.serializers.optional.bokeh import bokeh_serializers, bokeh_serializers_by_type
-from misen.utils.serde.serializers.optional.catboost import catboost_serializers, catboost_serializers_by_type
-from misen.utils.serde.serializers.optional.docx import docx_serializers, docx_serializers_by_type
-from misen.utils.serde.serializers.optional.faiss import faiss_serializers, faiss_serializers_by_type
-from misen.utils.serde.serializers.optional.geopandas import geopandas_serializers, geopandas_serializers_by_type
-from misen.utils.serde.serializers.optional.graphviz import graphviz_serializers, graphviz_serializers_by_type
-from misen.utils.serde.serializers.optional.hf_datasets import hf_datasets_serializers, hf_datasets_serializers_by_type
-from misen.utils.serde.serializers.optional.jax import jax_serializers, jax_serializers_by_type
-from misen.utils.serde.serializers.optional.keras import keras_serializers, keras_serializers_by_type
-from misen.utils.serde.serializers.optional.lightgbm import lightgbm_serializers, lightgbm_serializers_by_type
-from misen.utils.serde.serializers.optional.lxml import lxml_serializers, lxml_serializers_by_type
-from misen.utils.serde.serializers.optional.matplotlib import matplotlib_serializers, matplotlib_serializers_by_type
 from misen.utils.serde.serializers.optional.msgspec_struct import (
     msgspec_struct_serializers,
     msgspec_struct_serializers_by_type,
 )
-from misen.utils.serde.serializers.optional.networkx import networkx_serializers, networkx_serializers_by_type
 from misen.utils.serde.serializers.optional.numpy import numpy_serializers, numpy_serializers_by_type
-from misen.utils.serde.serializers.optional.onnx import onnx_serializers, onnx_serializers_by_type
-from misen.utils.serde.serializers.optional.openpyxl import openpyxl_serializers, openpyxl_serializers_by_type
 from misen.utils.serde.serializers.optional.pandas import pandas_serializers, pandas_serializers_by_type
 from misen.utils.serde.serializers.optional.pillow import pillow_serializers, pillow_serializers_by_type
-from misen.utils.serde.serializers.optional.plotly import plotly_serializers, plotly_serializers_by_type
 from misen.utils.serde.serializers.optional.polars import polars_serializers, polars_serializers_by_type
-from misen.utils.serde.serializers.optional.pptx import pptx_serializers, pptx_serializers_by_type
 from misen.utils.serde.serializers.optional.pyarrow import pyarrow_serializers, pyarrow_serializers_by_type
 from misen.utils.serde.serializers.optional.pydantic import pydantic_serializers, pydantic_serializers_by_type
-from misen.utils.serde.serializers.optional.pydub import pydub_serializers, pydub_serializers_by_type
 from misen.utils.serde.serializers.optional.scipy import scipy_serializers, scipy_serializers_by_type
-from misen.utils.serde.serializers.optional.shapely import shapely_serializers, shapely_serializers_by_type
-from misen.utils.serde.serializers.optional.sklearn import sklearn_serializers, sklearn_serializers_by_type
-from misen.utils.serde.serializers.optional.spacy import spacy_serializers, spacy_serializers_by_type
-from misen.utils.serde.serializers.optional.sympy import sympy_serializers, sympy_serializers_by_type
-from misen.utils.serde.serializers.optional.tensorflow import tensorflow_serializers, tensorflow_serializers_by_type
-from misen.utils.serde.serializers.optional.tokenizers import tokenizers_serializers, tokenizers_serializers_by_type
 from misen.utils.serde.serializers.optional.torch import torch_serializers, torch_serializers_by_type
-from misen.utils.serde.serializers.optional.transformers import (
-    transformers_serializers,
-    transformers_serializers_by_type,
-)
-from misen.utils.serde.serializers.optional.xarray import xarray_serializers, xarray_serializers_by_type
-from misen.utils.serde.serializers.optional.xgboost import xgboost_serializers, xgboost_serializers_by_type
 
 __all__ = ["optional_serializers", "optional_serializers_by_type"]
 
@@ -57,95 +22,28 @@ optional_serializers: SerializerTypeList = [
     *msgspec_struct_serializers,
     *pydantic_serializers,
     *attrs_serializers,
-    # Scientific / multidimensional
-    *xarray_serializers,
-    *sympy_serializers,
     # Numeric arrays
-    *jax_serializers,
     *numpy_serializers,
-    *tensorflow_serializers,
     *torch_serializers,
     # Tabular data
     *pandas_serializers,
     *polars_serializers,
     *pyarrow_serializers,
-    *geopandas_serializers,
-    # Sparse / specialized
+    # Sparse
     *scipy_serializers,
-    # Geospatial
-    *shapely_serializers,
-    # Graphs
-    *networkx_serializers,
-    *graphviz_serializers,
-    # ML models
-    *keras_serializers,
-    *sklearn_serializers,
-    *xgboost_serializers,
-    *lightgbm_serializers,
-    *catboost_serializers,
-    *onnx_serializers,
-    *faiss_serializers,
-    # NLP / tokenization
-    *spacy_serializers,
-    *tokenizers_serializers,
-    # HuggingFace
-    *transformers_serializers,
-    *hf_datasets_serializers,
-    # Audio
-    *pydub_serializers,
-    # Visualization
-    *altair_serializers,
-    *bokeh_serializers,
-    *matplotlib_serializers,
-    *plotly_serializers,
+    # Images
     *pillow_serializers,
-    # Documents
-    *openpyxl_serializers,
-    *docx_serializers,
-    *pptx_serializers,
-    # Web / markup
-    *beautifulsoup_serializers,
-    *lxml_serializers,
 ]
 
 optional_serializers_by_type: SerializerTypeRegistry = {
     **msgspec_struct_serializers_by_type,
     **pydantic_serializers_by_type,
     **attrs_serializers_by_type,
-    **xarray_serializers_by_type,
-    **sympy_serializers_by_type,
-    **jax_serializers_by_type,
     **numpy_serializers_by_type,
-    **tensorflow_serializers_by_type,
     **torch_serializers_by_type,
     **pandas_serializers_by_type,
     **polars_serializers_by_type,
     **pyarrow_serializers_by_type,
-    **geopandas_serializers_by_type,
     **scipy_serializers_by_type,
-    **shapely_serializers_by_type,
-    **networkx_serializers_by_type,
-    **graphviz_serializers_by_type,
-    **keras_serializers_by_type,
-    **sklearn_serializers_by_type,
-    **xgboost_serializers_by_type,
-    **lightgbm_serializers_by_type,
-    **catboost_serializers_by_type,
-    **onnx_serializers_by_type,
-    **faiss_serializers_by_type,
-    **spacy_serializers_by_type,
-    **tokenizers_serializers_by_type,
-    **transformers_serializers_by_type,
-    **hf_datasets_serializers_by_type,
-    **pydub_serializers_by_type,
-    **altair_serializers_by_type,
-    **bokeh_serializers_by_type,
-    **matplotlib_serializers_by_type,
-    **plotly_serializers_by_type,
     **pillow_serializers_by_type,
-    **openpyxl_serializers_by_type,
-    **docx_serializers_by_type,
-    **pptx_serializers_by_type,
-    **beautifulsoup_serializers_by_type,
-    **lxml_serializers_by_type,
 }
